5d9b8d07d7e71d09019efff7e447fc0b0f700e47
Denisse Rivas <iqrivas@gmail.com>
2019-08-22 21:46:29 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Typography variant="h2" align="center" gutterBottom>
+        To-Do List
+      </Typography>
+      <Grid container justify="center">
+        <Grid item>
+          <TextField type="text" placeholder="Add todo..." margin="normal" />
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+        <Grid item md={8}>
+          <List>
+            <ListItem button>
+              <Checkbox/>
+                <ListItemText primary="Check the prework"/>
+                <ListItemSecondaryAction>
+                  <IconButton>
+                    <DeleteIcon/>
+                  </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
